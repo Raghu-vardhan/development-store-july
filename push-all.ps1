339131e79ca4f243c1b0432db2d5a3c81973ffd2
Raghu-vardhan <rvraghu14@gmail.com>
2025-07-06 22:18:34 +0530

    new updates

--- push-all.ps1
+++ push-all.ps1
@@ -2,7 +2,7 @@
 # Push to Git
 git add .
 git commit -m "new updates"
-git push origin master  # or 'main', depending on your branch name
+git push  # or 'main', depending on your branch name
 
 # Push to Shopify
 shopify theme push
