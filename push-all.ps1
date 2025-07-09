8aea887b7c6b9adfa96cf8563c763f57af9cb637
Raghu-vardhan <rvraghu14@gmail.com>
2025-07-09 22:26:48 +0530

    new updates

--- push-all.ps1
+++ push-all.ps1
@@ -4,3 +4,7 @@
 git add .
 git commit -m "new updates"
 git push  # or 'main', depending on your branch name
+
+
+# <input type="radio" id="template--24927775424824__main-1-0" name="Subscription Type-1" value="Single Subscription" form="product-form-template--24927775424824__main" checked="" data-product-url="" data-option-value-id="6187875959096">
+# selectVariant(event, '50724727226680', 0)
